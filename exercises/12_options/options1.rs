2c5357d2f6f500e551d9855de23f5a4eda6d5de1
lightx0x <kingcairo4000@gmail.com>
2026-06-29 07:52:02 +0100

    Solve 12_options

--- exercises/12_options/options1.rs
+++ exercises/12_options/options1.rs
@@ -4,10 +4,30 @@
 // `hour_of_day` is higher than 23.
 fn maybe_ice_cream(hour_of_day: u16) -> Option<u16> {
     // TODO: Complete the function body.
+    match hour_of_day {
+        0..=21 => Some(5),
+        22..=23 => Some(0),
+        _ => None
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    let time_now_22: u16 = 22;
+    let time_now_23: u16 = 23;
+    let time_now_24: u16 = 24;
+    let time_now_14: u16 = 14;
+    let time_now_04: u16 = 4;
+    println!("How much icecream is left at {time_now_22}:00 => {:?} icecream is left"
+        , maybe_ice_cream(time_now_22).unwrap());
+    println!("How much icecream is left at {time_now_23}:00 => {:?} icecream is left"
+        , maybe_ice_cream(time_now_23).unwrap());
+    println!("How much icecream is left at {time_now_24}:00 => {:?} icecream is left"
+        , maybe_ice_cream(time_now_24).unwrap_or_default());
+    println!("How much icecream is left at {time_now_14}:00 => {:?} icecream is left"
+        , maybe_ice_cream(time_now_14).unwrap());
+    println!("How much icecream is left at {time_now_04}:00 => {:?} icecream is left"
+        , maybe_ice_cream(time_now_04).unwrap());
 }
 
 #[cfg(test)]
@@ -18,7 +38,7 @@ mod tests {
     fn raw_value() {
         // TODO: Fix this test. How do you get the value contained in the
         // Option?
-        let ice_creams = maybe_ice_cream(12);
+        let ice_creams = maybe_ice_cream(12).unwrap();
 
         assert_eq!(ice_creams, 5); // Don't change this line.
     }
