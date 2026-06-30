043bef5dbbc8d49395fd5b37fd03aa87fe6fa47d
lightx0x <kingcairo4000@gmail.com>
2026-06-30 12:18:04 +0100

    Solve 16_lifetimes & 17_tests

--- exercises/17_tests/tests1.rs
+++ exercises/17_tests/tests1.rs
@@ -13,11 +13,16 @@ fn main() {
 mod tests {
     // TODO: Import `is_even`. You can use a wildcard to import everything in
     // the outer module.
+    use super::*;
 
     #[test]
+
     fn you_can_assert() {
         // TODO: Test the function `is_even` with some values.
-        assert!();
-        assert!();
+        let even = is_even(10); 
+        let not_even = is_even(21); 
+        
+        assert!(even);
+        assert!(!not_even, "not_even wasn't true!");
     }
 }
